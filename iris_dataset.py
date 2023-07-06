1ff10a2020a850a42d5a34b4903f51185612aded
Sumitb2015 <sumitb2015@gmail.com>
2023-07-06 18:08:10 +0530

    Initial Commit

---
+++ iris_dataset.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score,recall_score,precision_score,mean_squared_error, r2_score,confusion_matrix, classification_report, roc_auc_score
+from sklearn.model_selection import train_test_split, cross_val_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_wine
+#Classification Models
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# Load the dataset
+features,target = load_wine(return_X_y=True,as_frame=True)
+
+# Preprocess the data
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=42)
+
+# Create the model
+model = GradientBoostingClassifier()
+# Train the model using the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
+
+# Calculate the confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print('Confusion Matrix:')
+print(cm)
+
+# Calculate the classification report
+cr = classification_report(y_test, y_pred)
+print('Classification Report:')
+print(cr)
